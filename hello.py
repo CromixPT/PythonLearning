4a908c55bcdd3e693326603edf9cfc7b8491c23f
CromixPT <tiago.g.candeias@gmail.com>
2019-08-02 23:03:44 +0100

    VSCode config settings

--- hello.py
+++ hello.py
@@ -1,12 +1,23 @@
-print("Hello ")
+import sys
 
+x = 1
+
+print("Hello ")
 x = 5
 y = 10
-
 print(x)
 print(y)
-
 y += x
-
+if y > 2:
+    print("bad")
 print(y)
 
+test = "teste"
+print("Hello")
+print("teste")
+
+
+def greet(who_to_meet):
+
+    greeting = "Hello, {}".format(who_to_meet)
+    return greeting
---
+++ hospital.py
@@ -0,0 +1,5 @@
+patient_name = "John Smith"
+age = 20
+is_new_patient = False
+
+print(patient_name + " " + str(age) + " " + str(is_new_patient))
